9ee6566600742ffe2db41ded1552074b33841a0d
Readerstacks <94598275+Readerstacks@users.noreply.github.com>
2023-07-07 13:03:54 +0000

    Update 8 files

--- app/page.js
+++ app/page.js
@@ -1,9 +1,31 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import { ApiService } from "../services/ApiService";
+import Plan from "./components/plan";
 
-export default function Home() {
+export default async function Home() {
+  const data = await ApiService.getProducts();
+
+  function hasPlans(plans) {
+    return (
+      plans &&
+      plans.data &&
+      plans.data.electricity &&
+      plans.data.electricity.length
+    );
+  }
+
   return (
-     <h1>Hello, Next.js!</h1>
+    <>
+      <h1>Electiricity Plans</h1>
 
-  )
+      {hasPlans(data) &&
+        data.data.electricity.map((plan) => {
+          return (
+            <>
+              <Plan item={plan} />
+            </>
+          );
+        })}
+      {!hasPlans(data) && <div>No Data found...</div>}
+    </>
+  );
 }
